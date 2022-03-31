e2574b8178db4e4b7384d6aa0955e72956dcde59
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-31 19:35:19 -0300

    finish jwt module

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,14 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-function sign(data, expires = "7d") {
-  return jwt.sign(data.toJSON(), process.env.APP_KEY, {
+const sign = (data, expires = "7d") => {
+  return jwt.sign(data.toJSON(), process.env.JWT_SECRET_KEY, {
     expiresIn: expires,
   });
 }
 
-function verify(token) {
-  const data = jwt.verify(token, process.env.APP_KEY);
+const verify = (token) => {
+  const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
   return data;
 }
 
-module.exports = { sign, verify };
+const signUser = (user, expires = '7d') => {
+  return sign({
+    id: user.id,
+    givenName: user.givenName,
+    bloodType: user.bloodType,
+    email: user.email,
+    name: user.name,
+    mainRole: user.isAdmin ? 'admin' : 'user'
+  }, expires);
+}
+
+module.exports = { sign, signUser, verify };
