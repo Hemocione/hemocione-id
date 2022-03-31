a668ea5a4b71d4f91c500a115f37e84472d0206a
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-03-31 19:35:28 -0300

    done with userService for now

--- src/services/models/userService.js
+++ src/services/models/userService.js
@@ -1,15 +1,24 @@
 const { user } = require('../../db/models');
 const { generateHashPassword, compareHashPassword } = require('../../utils/hash');
-import { UserNotFoundError, InvalidPasswordError } from "../../errors/AuthErrors";
+import { UserNotFoundError, InvalidPasswordError, UserRegistrationError } from "../../errors/AuthErrors";
+const { ValidationError } = require('sequelize');
 
-const registerUser = async (userData) => {
+const register = async (userData) => {
   const desiredData = { givenName, surName, document, phone, bloodType, birthDate, email, password, gender, image }
   const filteredUserData = ((desiredData) => (desiredData))(userData);
-  
-  return await user.create({
-    ...filteredUserData,
-    password: await generateHashPassword(filteredUserData.password)
-  })
+  try {
+    const createdUser = await user.create({
+      ...filteredUserData,
+      password: await generateHashPassword(filteredUserData.password)
+    })
+    return createdUser;
+  } catch (e) {
+    // if user is trying to register with an already registered email or CPF or invalid data
+    if (e instanceof ValidationError) {
+      throw new UserRegistrationError(e.errors[0].message);
+    }
+    throw e;
+  }
 }
 
 const login = async (email, password) => {
