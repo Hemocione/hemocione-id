132c72ad1e8b2b9bff6ca80614252de9a11ff617
Thiago Guimarães <45672975+guim4dev@users.noreply.github.com>
2022-06-22 16:02:16 -0300

    Add useless comment do force deploy

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || process.env.SERVER_PORT || 8080;
 
+// load express
 const end = loader({
   expressApp: app,
   isTest: process.env.ENV === "test" ? true : false,
