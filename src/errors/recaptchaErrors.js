b631530fef51e797e882d7435dd37fcb702e7de4
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2022-07-12 21:20:46 -0300

    add recaptcha shit on login and signup

---
+++ src/errors/recaptchaErrors.js
@@ -0,0 +1,15 @@
+class RecaptchaError extends CustomAPIError {
+  constructor(name, message, statusCode) {
+    super(name, message, statusCode, 'RecaptchaError')
+  }
+}
+
+class InvalidRecaptchaError extends RecaptchaError {
+  constructor(message = 'Erro de captcha. Você é um robô?') {
+    super('InvalidRecaptchaError', message, 403)
+  }
+}
+
+module.exports = {
+  InvalidRecaptchaError,
+}
---
+++ src/services/recaptchaService.js
@@ -0,0 +1,19 @@
+const { InvalidRecaptchaError } = require('../errors/recaptchaErrors')
+
+const validateRecaptcha = async (recaptchaToken) => {
+  try {
+    const googleRes = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
+      method: 'POST',
+      body: `secret=${process.env.SECRET_RECAPTCHA_KEY}&response=${recaptchaToken}`,
+      headers: { "Content-Type": 'application/x-www-form-urlencoded' }
+    })
+    const googleResJson = await googleRes.json()
+    if (!googleResJson['success']) {
+      throw new InvalidRecaptchaError()
+    }
+  } catch(error) {
+    throw new InvalidRecaptchaError()
+  }
+}
+
+module.exports = { validateRecaptcha };
