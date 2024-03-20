03caa94a37b519d531f03a02dc2acc69c0cc5bfb
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2024-03-20 17:27:03 -0300

    minorfix to bugSnag loading

--- src/middlewares/bugsnag.js
+++ src/middlewares/bugsnag.js
@@ -1,13 +1,13 @@
 const Bugsnag = require('@bugsnag/js')
 const BugsnagPluginExpress = require('@bugsnag/plugin-express')
 
-const bugsnag = Bugsnag.start({
+Bugsnag.start({
   apiKey: process.env.BUGSNAG_API_KEY,
   plugins: [BugsnagPluginExpress],
   enabledReleaseStages: ['production', 'development'],
   releaseStage: process.env.ENV || 'local',
 })
 
-const bugsnagMiddlewares = bugsnag.getPlugin('express')
+const bugsnagMiddlewares = Bugsnag.getPlugin('express')
 
 module.exports = bugsnagMiddlewares
